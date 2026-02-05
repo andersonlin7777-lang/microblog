aca53606cb89822c8420239593f18b56d729db7e
anderson <andersonlin7777@gmail.com>
2026-02-05 22:19:43 +0800

    posts table

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
@@ -14,8 +15,21 @@ class User(db.Model):
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+
+    posts: so.WriteOnlyMapped["Posts"] = so.relationship(back_populates="author")
     
     #Python 方法。終端機印出一個 User 物件時，不會顯示難懂的 <User object at 0x...>
     #而是顯示清晰的 <User sally>。這對Debugging有幫助
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+
+class Posts(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    body: so.Mapped[str] = so.mapped_column(sa.String(140))
+    timestamp: so.Mapped[datetime] = so.mapped_column(index=True, default=lambda: datetime.now(timezone.utc))
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), index=True)
+    author: so.Mapped[User] = so.relationship(back_populates="posts")
+
+    def __repr__(self):
+        return '<Posts {}>'.format(self.body)
