01c49b7dc30c638e9cbe42700584774b66e3c697
andersonlin7777-lang <andersonlin7777@gmail.com>
2026-03-13 16:21:47 +0800

    add follow and unfollow

--- app/forms.py
+++ app/forms.py
@@ -45,6 +45,11 @@ class EditProfileForm(FlaskForm):
             if user is not None:
                 raise ValidationError("Please use a different username.")
             
+class EmptyForm(FlaskForm):
+    submit = SubmitField("Submit")
+
+
+            
 
 
 
